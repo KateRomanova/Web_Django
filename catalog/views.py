937c86d685cb9971ccf444463a7006965d5a3d77
KateRomanova <kate.romanova@gmail.com>
2024-05-23 14:11:02 +0300

    templates and utils added Post get added

--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} ({phone}): {message}')
+
+    return render(request, 'contacts.html')
