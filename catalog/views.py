b4e133796744f9cabceed00edb15ee6aa1fbeac0
KateRomanova <kate.romanova@gmail.com>
2024-06-11 18:29:06 +0300

    commands added

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
@@ -13,3 +15,15 @@ def contacts(request):
         print(f'{name} ({phone}): {message}')
 
     return render(request, 'contacts.html')
+
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'product_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
