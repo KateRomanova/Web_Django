cc8a3618e6552909fd4c35376f66d62ade0ffa1a
KateRomanova <kate.romanova@gmail.com>
2024-06-29 19:05:30 +0300

    commented code deleted

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, CreateView
 
 from catalog.models import Product
 
@@ -12,31 +12,16 @@ class ContactsView(TemplateView):
         context['contacts'] = Product.objects.all()[:5]
         return context
 
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(f'{name} ({phone}): {message}')
-#
-#     return render(request, 'contacts.html')
-
 
 class ProductListView(ListView):
     model = Product
-    # app_name/<model_name>_<action>.html
-    # catalog/product_list.html
-
-# def product_list(request):
-#     products = Product.objects.all()
-#     context = {'products': products}
-#     return render(request, 'product_list.html', context)
 
 
 class ProductDetailView(DetailView):
     model = Product
 
-# def product_detail(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     context = {'product': product}
-#     return render(request, 'product_detail.html', context)
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = '__all__'
+    success_url = reverse_lazy('catalog:product_list')
